85d993ac633e90121f57179b48dbebc1a36b0b26
geospiza <geospiza@protonmail.com>
2021-02-06 13:35:32 -0800

    Add a popup with svelte stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,10 @@
 import copy from "rollup-plugin-copy-watch";
+import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import css from "rollup-plugin-css-only";
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
@@ -7,8 +13,23 @@ export default [
     plugins: [
       copy({
         targets: [{ src: "static/*", dest: "dist" }],
-        watch: "static",
+        watch: production ? null : "static",
       }),
+    ],
+  },
+  {
+    input: "src/popup.js",
+    output: {
+      sourcemap: true,
+      format: "iife",
+      name: "app",
+      file: "dist/popup/bundle.js",
+    },
+    plugins: [
+      svelte({ compilerOptions: { dev: !production } }),
+      css({ output: "bundle.css" }),
+      resolve({ browser: true, dedupe: ["svelte"] }),
+      commonjs(),
     ],
   },
 ];
---
+++ src/Popup.svelte
@@ -0,0 +1,10 @@
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
+
+<main>
+  <h1>Hello World!</h1>
+  <a href="https://maplelegends.com">Go to MapleLegends</a>
+</main>
