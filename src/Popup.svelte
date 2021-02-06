8118e02ee1cca2b47a432fa4dd81fe6d174f0f54
geospiza <geospiza@protonmail.com>
2021-02-06 15:27:15 -0800

    Add popup page that shows logged in info

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,10 +1,42 @@
-<style>
-  h1 {
-    text-align: center;
+<script>
+  import { onMount } from "svelte";
+
+  let loggedin = false;
+  let accountDetails;
+  let characters;
+
+  function parseAccountCharacters(content) {
+    return [...content.querySelectorAll(".spa")]
+      .filter(node => node.innerText)
+      .map(node => node.innerText);
   }
-</style>
+
+  onMount(async () => {
+    let resp = await fetch("https://maplelegends.com/my/account");
+    let html = await resp.text();
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, "text/html");
+    loggedin = doc.querySelector("#loginform") == null;
+    accountDetails = doc.querySelector("#page-content");
+    if (loggedin && accountDetails) {
+      characters = parseAccountCharacters(accountDetails);
+    }
+  });
+</script>
 
 <main>
-  <h1>Hello World!</h1>
   <a href="https://maplelegends.com">Go to MapleLegends</a>
+  {#if loggedin}
+    {#if characters}
+      <h2>Character List</h2>
+      <ul>
+        {#each characters as char}
+          <li>{char}</li>
+        {/each}
+      </ul>
+    {/if}
+    {@html accountDetails.innerHTML}
+  {:else}
+    <p>Not logged in.</p>
+  {/if}
 </main>
