667f51fe3d83e01738b017b6426c20bfea1c5a1b
geospiza <geospiza@protonmail.com>
2021-02-07 01:34:14 -0800

    Fix images and make chrome compatible

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 // Without this background script, there are issues when trying to insert the
 // plot on navigation.
 // https://stackoverflow.com/a/21071357
-browser.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-  browser.tabs.executeScript(null, { file: "content-bundle.js" });
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  chrome.tabs.executeScript(null, { file: "content-bundle.js" });
 });
--- src/components/Popup.svelte
+++ src/components/Popup.svelte
@@ -19,14 +19,21 @@
     accountDetails = doc.querySelector("#page-content");
     // replace links in account details so they point to the maplelegends base
     for (let node of accountDetails.querySelectorAll("a")) {
+      // always open in a new tab
+      node.target = "_blank";
       node.href = `${BASE_URL}/${node.href.split("/").reverse()[0]}`;
+    }
+    // also fix images, we add mapleio to the list of valid urls
+    // which are coming from the api. We do something hacky here...
+    for (let node of accountDetails.querySelectorAll("img")) {
+      node.src = `${BASE_URL}/api/${node.src.split("/").reverse()[0]}`;
     }
   });
 </script>
 
 <main>
   <h1>
-    <a href="https://maplelegends.com">MapleLegends</a>
+    <a href="https://maplelegends.com" target="_blank">MapleLegends</a>
   </h1>
   <h2>Server Info</h2>
   {#if serverStatus && uniqueUsers}
