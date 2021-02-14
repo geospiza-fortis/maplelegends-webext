71b486f2dc90100c5598338fbef5b4524a96a1c5
geospiza <geospiza@protonmail.com>
2021-02-14 14:04:02 -0800

    Update the about page and fix addon width

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -9,8 +9,17 @@
   ).
 </p>
 
+<p>Thank you to the staff at MapleLegends for fostering a great community.</p>
+
 <p>
-  Find the source at
+  This webextension was written by
+  <a href="https://geospiza.me/">geospiza.</a>
+  Leave
+  <a
+    href="https://forum.maplelegends.com/index.php?threads/maplelegends-web-extension.37188/#post-250036">
+    feedback on the forum post.
+  </a>
+  Find the source on Github at
   <a
     href="https://github.com/geospiza-fortis/maplelegends-webext"
     target="_blank">
